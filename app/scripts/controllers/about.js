88aa00c0343c16ce4ce6839dcbec770f1a402a18
quixote15 <santos2tiago@gmail.com>
2018-04-05 18:23:53 -0300

    Generate yo project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chatDevApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the chatDevApp
+ */
+angular.module('chatDevApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
